cc240493cbab79cfe750e04554ca747f363389b5
Fabio Mosca <fabio.mosca.cs@gmail.com>
2019-11-17 20:33:36 +0100

    Some image tracking

---
+++ Assets/Scripts/Custom/ImageRecognitionSample.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognitionSample : MonoBehaviour
+{
+    private ARTrackedImageManager m_ArTrackedImageManager;
+
+    private void Awake()
+    {
+        m_ArTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        m_ArTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        m_ArTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
